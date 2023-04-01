e22de90c61557eef599ce08d53b1a34f4be2e253
MCapelanes <matheus.capelanes@outlook.com>
2023-04-01 13:14:23 -0300

    criando componente de card

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Tags from '../Tags'
+import styles from './Galeria.module.scss'
+import fotos from './fotos.json'
+import Cards from './Cards'
 
 export default function Galeria() {
   return (
-    <section>
+    <section className={styles.galeria}>
         <h2>Navegue pela galeria</h2>
         <Tags />
-        <ul></ul>
+        <Cards itens={fotos} styles={styles} />
     </section>
   )
 }
