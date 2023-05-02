7331dacb28f8020fc377656e6afe8837369ebecc
MCapelanes <matheus.capelanes@outlook.com>
2023-05-02 17:58:27 -0300

    adicionando fotos

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -3,13 +3,14 @@ import search from './search.png'
 import styles from './Cabecalho.module.scss'
 
 export default function Cabecalho(){
-    return(
-        <header className={styles.cabecalho}>
-            <img src={logo} alt="Logo do Alura Space"/>
-            <div className={styles.cabecalho__container}>
-                <input className={styles.cabecalho__input} type="text" placeholder="O que voce procura ?"/>
-                <img src={search} alt="Icone de Lupa"/>
-            </div>
-        </header>
-    )
+  return (
+    <header className={styles.cabecalho}>
+      <img src={logo} alt="Logo do Alura Space" />
+      <div className={styles.cabecalho__container}>
+        <input className={styles.cabecalho__input} type="text"
+        placeholder="O que você procura?" />
+        <img src={search} alt="ícone de lupa" />
+      </div>
+    </header>
+  )
 }
--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,17 +1,28 @@
-import Banner from "../../componentes/Banner";
+import banner from "./banner.png";
 import Cabecalho from "../../componentes/Cabecalho";
-import Galeria from "../../componentes/Galeria";
+import Menu from "../../componentes/Menu";
+import styles from "./PaginaInicial.module.scss";
 import Rodape from "../../componentes/Rodape";
-
+import Galeria from "../../componentes/Galeria";
+import Populares from "../../componentes/Populares";
 
 export default function PaginaInicial() {
   return (
     <>
       <Cabecalho />
-      <Banner />
-      <div>
-        <Galeria />
-      </div>
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
       <Rodape />
     </>
   );
